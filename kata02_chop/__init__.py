69b4a20f5176923aae328cb957649f11f9ef47c3
Pedro Calleja <pecalleja@gmail.com>
2021-06-04 19:48:15 -0500

    divide tests

--- kata02_chop/__init__.py
+++ kata02_chop/__init__.py
@@ -1,4 +1,15 @@
+#
+# http://codekata.com/kata/kata02-karate-chop/
+#
+
+
 def chop(target: int, elements: list[int]) -> int:
+    """
+    :param target: the integer to search into elements array
+    :param elements: ordered list of elements
+
+    :return: -1 if element not found or the index of the first occurrence
+    """
     if not elements:
         return -1
     try:
--- kata02_chop/test_karate_chop.py
+++ kata02_chop/test_karate_chop.py
@@ -13,19 +13,21 @@ def test_found_target_simple():
     assert 0 == chop(1, [1])
 
 
-def test_all_others_i_have_not_time_for_this():
+def test_found_element_full_array():
     assert 0 == chop(1, [1, 3, 5])
     assert 1 == chop(3, [1, 3, 5])
     assert 2 == chop(5, [1, 3, 5])
-    assert -1, chop(0, [1, 3, 5])
-    assert -1, chop(2, [1, 3, 5])
-    assert -1, chop(4, [1, 3, 5])
-    assert -1, chop(6, [1, 3, 5])
-    #
     assert 0 == chop(1, [1, 3, 5, 7])
     assert 1 == chop(3, [1, 3, 5, 7])
     assert 2 == chop(5, [1, 3, 5, 7])
     assert 3 == chop(7, [1, 3, 5, 7])
+
+
+def test_not_found_full_array():
+    assert -1, chop(0, [1, 3, 5])
+    assert -1, chop(2, [1, 3, 5])
+    assert -1, chop(4, [1, 3, 5])
+    assert -1, chop(6, [1, 3, 5])
     assert -1, chop(0, [1, 3, 5, 7])
     assert -1, chop(2, [1, 3, 5, 7])
     assert -1, chop(4, [1, 3, 5, 7])
