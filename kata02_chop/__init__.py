d719e106ebcb3b53136a0310b7dc039dcb60249c
Pedro Calleja <pecalleja@gmail.com>
2021-06-06 13:18:28 -0500

    fourth approach

--- kata02_chop/__init__.py
+++ kata02_chop/__init__.py
@@ -11,11 +11,28 @@ def chop(target: int, elements: list[int], index: int = 0) -> int:
 
     :return: -1 if element not found or the index of the first occurrence
     """
-    last = None
-    for key, value in enumerate(elements):
-        if last and last > value:
+    if not elements:
+        return -1
+    elements_count = len(elements)
+    if elements_count == 1:
+        if elements[0] == target:
+            return index
+        else:
             return -1
-        if value == target:
-            return key
-        last = value
-    return -1
+    center = elements_count // 2
+
+    # lets supouse that the input array is ordered
+    if elements[center - 1] > elements[center]:
+        return -1
+
+    first_half = chop(target, elements[0:center], index)
+    if first_half < 0:
+        second_half = chop(
+            target, elements[center:elements_count], index + center
+        )
+        if second_half < 0:
+            return -1
+        else:
+            return second_half
+    else:
+        return first_half
